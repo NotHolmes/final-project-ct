291e532f4060d9ac81b62074b51ea2e7af60abc1
Zahil Chuduang <zahil.c@ku.th>
2022-10-27 12:51:31 +0700

    add update and fix destroy

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -25,11 +25,35 @@ class PostController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post();
+        $post->title = $request->get('title');
+        $post->image = $request->get('image');
+        $post->description = $request->get('description');
+        $post->published_at = now();
+
+        if($request->has('reward')) {
+            $post->reward = $request->get('reward');
+        }
+
+        $post->is_lost = $request->get('is_lost');
+        $post->latitude = $request->get('latitude');
+        $post->longitude = $request->get('longitude');
+
+        if ($post->save()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Post saved successfully with id ' . $post->id,
+                'post_id' => $post->id
+            ], Response::HTTP_CREATED);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => 'Post saved failed'
+        ], Response::HTTP_BAD_REQUEST);
     }
 
     /**
@@ -52,16 +76,25 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        $title = $post->title;
-        if ($post->delete()) {
+        if($request->has('title')) $post->title = $request->get('title');
+        if($request->has('image')) $post->image = $request->get('image');
+        if($request->has('description')) $post->description = $request->get('description');
+        if($request->has('published_at')) $post->published_at = $request->get('published_at');
+        if($request->has('reward')) $post->reward = $request->get('reward');
+        if($request->has('is_lost')) $post->is_lost = $request->get('is_lost');
+        if($request->has('latitude')) $post->latitude = $request->get('latitude');
+        if($request->has('longitude')) $post->longitude = $request->get('longitude');
+
+        if ($post->save()) {
             return response()->json([
                 'success' => true,
-                'message' => "Reward {$title} deleted successfully"
+                'message' => 'Post updated successfully with id ' . $post->id,
+                'post_id' => $post->id
             ], Response::HTTP_OK);
         }
         return response()->json([
             'success' => false,
-            'message' => "Reward {$title} deleted failed"
+            'message' => 'Post saved failed'
         ], Response::HTTP_BAD_REQUEST);
     }
 
@@ -69,10 +102,20 @@ class PostController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Post $post)
     {
-        //
+        $title = $post->title;
+        if ($post->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => "Post {$title} deleted successfully"
+            ], Response::HTTP_OK);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => "Post {$title} deleted failed"
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
