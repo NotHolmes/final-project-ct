e6aa196fb089adc0e82b0c32d1b24a9b17046775
Zahil Chuduang <zahil.c@ku.th>
2022-10-27 11:56:23 +0700

    first commit

---
+++ database/migrations/2022_10_27_011750_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->string('description');
+            $table->dateTime('published_at')->nullable()->default(null);
+            $table->unsignedDouble('reward')->nullable()->default(0);
+            $table->boolean('is_lost');
+            $table->boolean('is_done')->default(false);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
