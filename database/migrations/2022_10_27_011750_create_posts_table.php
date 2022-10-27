84c5baac603cbebc6fcad2303f6af00e8b581d8b
Zahil Chuduang <zahil.c@ku.th>
2022-10-27 17:09:14 +0700

    add user_id to post migration

--- database/migrations/2022_10_27_011750_create_posts_table.php
+++ database/migrations/2022_10_27_011750_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class); // user_id
             $table->string('title');
             $table->string('image');
             $table->text('description');
