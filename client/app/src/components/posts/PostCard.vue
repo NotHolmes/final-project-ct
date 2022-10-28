8eaccac5830352aca6d39f2401dee9199b7068a7
Zahil Chuduang <zahil.c@ku.th>
2022-10-28 19:53:17 +0700

    add route add /posts/DetailView.vue

--- client/app/src/components/posts/PostCard.vue
+++ client/app/src/components/posts/PostCard.vue
@@ -1,42 +1,11 @@
 <template>
-    <!--  <div class="block p-2 mt-4 border-2 border-blue-700 rounded">-->
-    <!--    <h3 class="text-xl">{{ reward.name }}</h3>-->
-    <!--    <p class="text-4xl">{{ reward.point }} point</p>-->
-
-    <!--    <div class="text-xl">-->
-    <!--      <slot name="total_amount"></slot>-->
-    <!--    </div>-->
-
-    <!--    <div class="">-->
-    <!--      <slot></slot>-->
-    <!--    </div>-->
-
-    <!--    <div class="grid grid-cols-2">-->
-    <!--      <button @click="onClickButton()"-->
-    <!--      class="p-2 mx-4 bg-blue-200 border-2 border-blue-400 rounded-xl">-->
-    <!--        Redeem-->
-    <!--      </button>-->
-    <!--      <div class="grid grid-cols-2">-->
-    <!--        <button @click="onClickLike()"-->
-    <!--        class="p-2 mx-4 bg-yellow-200 border-2 border-blue-400 rounded-xl">-->
-    <!--          {{ likeCount }} Likes-->
-    <!--        </button>-->
-
-    <!--        <button @click="onClickCounter()"-->
-    <!--        class="p-2 mx-8 bg-green-200 border-2 border-blue-400 rounded-xl">-->
-    <!--          {{ counter_store.counter }} |-->
-    <!--          {{ counter_store.doubleCount }}-->
-    <!--        </button> -->
-    <!--      </div>-->
-    <!--    </div>-->
-    <!--  </div>-->
     <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
         <a href="/" aria-label="Article"><img v-bind:src="post.image" class="object-cover w-full h-64 rounded" alt="" /></a>
         <div class="py-5">
             <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
                 {{ formatDate(post.published_at)}}
             </p>
-            <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"><p class="text-2xl font-bold leading-5">
+            <a @click="onClickButton()" aria-label="Article" class="cursor-pointer inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"><p class="text-2xl font-bold leading-5">
                 {{ post.title }}</p>
             </a>
             <p class="mb-4 text-gray-700">
---
+++ client/app/src/views/posts/DetailView.vue
@@ -0,0 +1,39 @@
+<template>
+
+    {{ post.id }}
+<!--    <div>-->
+<!--        <gmap-map-->
+<!--        :center="{lat:10, lng:10}"-->
+<!--        :zoom="7"-->
+<!--        style="width: 100%; height: 320px">-->
+
+<!--        </gmap-map>-->
+<!--        <script src="{{ mix('resources/app.js') }}" ></script>-->
+<!--    </div>-->
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: null,
+            error: null
+        }
+    },
+    async created() {
+        const id = this.$route.params.id
+        const url = `/posts/${id}`
+        try {
+            this.error = null
+            let response = await this.$axios.get(url)
+            if (response.status === 200) {
+                this.reward = response.data.data
+                console.table(this.post)
+            }
+        } catch (error) {
+            this.error = error.message
+        }
+    }
+}
+</script>
