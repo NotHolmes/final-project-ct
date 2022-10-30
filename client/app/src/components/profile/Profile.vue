55831f007b9cbc961ee5136aed3a9bbdec7fccac
chatchawan <chatchawan.sam@ku.th>
2022-10-31 01:40:27 +0700

    Fix Navbar

--- client/app/src/components/profile/Profile.vue
+++ client/app/src/components/profile/Profile.vue
@@ -4,7 +4,7 @@
         <li
             class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
         >
-            {{ auth.name }}
+            {{ auth.email }}
         </li>
 
         <li
