96a493ba90d2619b249fd52e56bd551a20c5644e
Zahil Chuduang <zahil.c@ku.th>
2022-11-02 13:48:32 +0700

    add search bar with filter and change text=false in .gitattrbutes

--- up.sh
+++ up.sh
@@ -1,7 +1,6 @@
 ./vendor/bin/sail up -d
 docker-compose -f ./client/docker-compose.yml up -d
 docker-compose -f ./client/server/docker-compose.yml up -d
-
 if [[ "$OSTYPE" =~ ^linux ]]
 then
 cmd.exe /c start cmd.exe /c wsl.exe docker-compose -f ./client/docker-compose.yml exec app npm run dev &
@@ -10,11 +9,9 @@ elif [[ "$OSTYPE" =~ ^darwin ]]
 then
 command1="docker-compose -f ./client/docker-compose.yml exec app npm run dev"
 command2="docker-compose -f ./client/server/docker-compose.yml exec app npm run start"
-
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
   -e "tell application \"Terminal\" to do script \"${command1};\" in selected tab of the front window"
-
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
   -e "tell application \"Terminal\" to do script \"${command2};\" in selected tab of the front window"
