f52dbb1b62729f40f3a44ed7638e27738fa431b8
NotHolmes <78583571+NotHolmes@users.noreply.github.com>
2022-11-02 12:24:19 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -2,21 +2,22 @@
 docker-compose -f ./client/docker-compose.yml up -d
 docker-compose -f ./client/server/docker-compose.yml up -d
 
-cmd.exe /c start cmd.exe /c wsl.exe docker-compose -f ./client/docker-compose.yml exec app npm run dev 2>/tmp/null
-cmd.exe /c start cmd.exe /c wsl.exe docker-compose -f ./client/server/docker-compose.yml exec app npm run start 2>/tmp/null
-
-if [[ $? != 0 ]]
+if [[ "$OSTYPE" =~ ^linux ]]
 then
-
+cmd.exe /c start cmd.exe /c wsl.exe docker-compose -f ./client/docker-compose.yml exec app npm run dev &
+cmd.exe /c start cmd.exe /c wsl.exe docker-compose -f ./client/server/docker-compose.yml exec app npm run start &
+elif [[ "$OSTYPE" =~ ^darwin ]]
+then
 command1="docker-compose -f ./client/docker-compose.yml exec app npm run dev"
 command2="docker-compose -f ./client/server/docker-compose.yml exec app npm run start"
 
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
-  -e "tell application \"Terminal\" to do script \"${command1};\" in selected tab of the front window";
+  -e "tell application \"Terminal\" to do script \"${command1};\" in selected tab of the front window"
 
 osascript -e 'tell application "Terminal" to activate' \
   -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
-  -e "tell application \"Terminal\" to do script \"${command2};\" in selected tab of the front window";
-
+  -e "tell application \"Terminal\" to do script \"${command2};\" in selected tab of the front window"
+else
+        exit 1
 fi
